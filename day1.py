f4583bae25490ceddd04eb485d5f78d1b550f26b
Rhys Fuller <rfull917@students.bju.edu>
2024-12-15 17:00:31 -0500

    upgrades to day 1

--- day1.py
+++ day1.py
@@ -1,16 +1,5 @@
 l1 = []
 l2 = []
-
-for i in range(1000):
-    x, y = input().split()
-    l1.append(int(x))
-    l2.append(int(y))
-
-l1.sort()
-l2.sort()
-
-distance = 0
-for i in range(1000):
-    distance += abs(l1[i] - l2[i])
-
-print(distance)
+[((inp:=input().split()), l1.append(int(inp[0])), l2.append(int(inp[1]))) for i in range(1000)]
+l1.sort(), l2.sort()
+print(sum([abs(l1[i] - l2[i]) for i in range(1000)]))
